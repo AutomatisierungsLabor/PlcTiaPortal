46eeee81219b0d801052d338a8ec9c93b4b04980
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2021-10-15 11:27:52 +0200

    Labor

--- FIO_FUP_Tank_01/ProjektStarten.cmd
+++ FIO_FUP_Tank_01/ProjektStarten.cmd
@@ -1,6 +1,3 @@
 h:
-del "%localappdata%\Real Games\Factory IO\*.*" /Q /S
-copy h:\TiaPortal\Factory_IO\config.cfg "%localappdata%\Real Games\Factory IO\config.cfg"
 cd h:\TiaPortal\Tank
-start h:\TiaPortal\Factory_IO\Tank.factoryio
 start Tank.ap14
